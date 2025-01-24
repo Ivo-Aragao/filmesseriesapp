30df4d8126105edad034d57ae8416fb9d4a5d8dc
ivo aragao <ivoaragao4@gmail.com>
2025-01-24 12:20:33 -0300

    feat: "Exportar funções utilitárias do TMDB para lançamentos, busca, gêneros e detalhes"

---
+++ src/api/tmdb.js
@@ -0,0 +1,12 @@
+import getLatestReleases from './getLatestReleases';
+import { searchMoviesAndSeries } from './searchMoviesAndSeries';
+import { getMoviesByGenre } from './getMoviesByGenre';
+import { getDetails } from './getDetails';
+
+// Exporta todas as funções do TMDB
+export {
+  getLatestReleases,
+  searchMoviesAndSeries,
+  getMoviesByGenre,
+  getDetails,
+};
