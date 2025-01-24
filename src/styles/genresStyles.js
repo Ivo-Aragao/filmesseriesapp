7a9f6d777412c20d9f2a12ec8968dc9c0ce56316
ivo aragao <ivoaragao4@gmail.com>
2025-01-24 12:26:12 -0300

    feat: "Adicionar estilos para a tela de Gêneros"

---
+++ src/styles/genresStyles.js
@@ -0,0 +1,17 @@
+// src/styles/genresStyles.js
+import { StyleSheet } from 'react-native';
+
+const genresStyles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 20,
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 20,
+    textAlign: 'center',
+  },
+});
+
+export default genresStyles;
